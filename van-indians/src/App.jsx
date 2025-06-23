7eea8cd178c8c737ee1a12c6fbd4e548954a1e5d
prateek-balani <prateekbalani03@gmail.com>
2025-06-22 21:01:09 -0700

    added navbar and footer to app.jsx

--- van-indians/src/App.jsx
+++ van-indians/src/App.jsx
@@ -1,24 +1,32 @@
 import { useState } from 'react'
 
 
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/home'
 import FAQ from './pages/FAQ'
 import Gallery from './pages/Gallery'
 import Events from './pages/Events'
+import Navbar from './components/navbar';
+import Footer from './components/footer';
 
 function App() {
-  return(
+  return (
     <Router>
+      <>
+     <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/gallery" element={<Gallery />} />
         <Route path="/faq" element={<FAQ />} />
         <Route path="/events" element={<Events />} />
       </Routes>
+      <Footer />
+       </>
     </Router>
+
+    
   );
-  
+
 }
 
 export default App;
--- van-indians/src/pages/Home.jsx
+++ van-indians/src/pages/Home.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import Navbar from '../components/navbar';
-import Footer from '../components/footer'
+
+
 import Carousel from '../components/carousel';
 
 const Home = () => {
     return(
       <>
-      <Navbar />
+     
       <Carousel />
-      <Footer />
+
       </>
 
         

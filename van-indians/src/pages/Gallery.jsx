b511319226fbab32a14c18f80a11241a28209246
prateek-balani <prateekbalani03@gmail.com>
2025-06-23 16:19:51 -0700

    gallery page template

--- van-indians/src/pages/Gallery.jsx
+++ van-indians/src/pages/Gallery.jsx
@@ -1,6 +1,41 @@
 import React from 'react';
 
 const Gallery = () => {
+    return (
+        <>
+            <div className="text-center py-10">
+                <h2 className="text-4xl font-bold mb-6">Community Gallery</h2>
+                <p className="text-gray-600 max-w-xl mx-auto">
+                    A glimpse into our events, meetups, and celebrations 🎉
+                </p>
+            </div>
+
+
+            <div className="grid gap-4 px-4 pb-10">
+                <div>
+                    <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/featured/image.jpg" alt="" />
+                </div>
+                <div class="grid grid-cols-5 gap-4">
+                    <div>
+                        <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" alt="" />
+                    </div>
+                    <div>
+                        <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" alt="" />
+                    </div>
+                    <div>
+                        <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" alt="" />
+                    </div>
+                    <div>
+                        <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg" alt="" />
+                    </div>
+                    <div>
+                        <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg" alt="" />
+                    </div>
+                </div>
+            </div>
+
+        </>
+    );
 
 }
 

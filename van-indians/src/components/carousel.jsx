bd47c208d6f6fec154d6c0fc7d251215d25dccad
prateek-balani <prateekbalani03@gmail.com>
2025-06-24 15:47:45 -0700

    changed the links and smaller fixes

--- van-indians/src/components/carousel.jsx
+++ van-indians/src/components/carousel.jsx
@@ -7,7 +7,7 @@ const Carousel = () => {
             <div className="carousel mx-auto mt-6 w-full max-w-4xl h-96 rounded-xl overflow-hidden shadow-md">
                 <div id="slide1" className="carousel-item relative w-full">
                     <img
-                        src="../public/img-1.jpg"
+                        src="/img-1.jpg"
                         className="w-full h-full object-cover" />
                     <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
                         <a href="#slide4" className="btn btn-circle">❮</a>
@@ -16,7 +16,7 @@ const Carousel = () => {
                 </div>
                 <div id="slide2" className="carousel-item relative w-full">
                     <img
-                        src="../public/img-2.jpg"
+                        src="/img-2.jpg"
                         className="w-full h-full object-cover" />
                     <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
                         <a href="#slide1" className="btn btn-circle">❮</a>
@@ -25,7 +25,7 @@ const Carousel = () => {
                 </div>
                 <div id="slide3" className="carousel-item relative w-full">
                     <img
-                        src="../public/img-3.jpg"
+                        src="/img-3.jpg"
                         className="w-full h-full object cover" />
                     <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
                         <a href="#slide2" className="btn btn-circle">❮</a>
@@ -34,7 +34,7 @@ const Carousel = () => {
                 </div>
                 <div id="slide4" className="carousel-item relative w-full">
                     <img
-                        src="../public/img-4.jpg"
+                        src="/img-4.jpg"
                         className="w-full h-full object cover" />
                     <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
                         <a href="#slide3" className="btn btn-circle">❮</a>

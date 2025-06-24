2eae2fa938f8bad9977185bd0130fd4969d8195c
prateek-balani <prateekbalani03@gmail.com>
2025-06-24 15:38:55 -0700

    fixed css and added images to events place

--- van-indians/src/pages/Events.jsx
+++ van-indians/src/pages/Events.jsx
@@ -4,20 +4,24 @@ const Events = () => {
         {
             title: "Brunch in Little India",
             description: "Do you love Indian Food? Come down to Little India and enjoy a wide variety of amazing food.",
+            imgSrc: "/img-4.jpg",
 
         },
         
             {
                 title: "Bollywood Dance Night",
                 description: "Join us for an evening of Bollywood music and dance. Show off your moves or learn some new ones!",
+                imgSrc: "/img-2.jpg",
             },
             {
                 title: "Cricket Tournament",
                 description: "Participate in our friendly cricket tournament or cheer for your favorite team. Fun for all ages!",
+                imgSrc: "/img-5.jpg",
             },
             {
                 title: "Diwali Celebration",
                 description: "Celebrate the festival of lights with us. Enjoy cultural performances, food, and fireworks!",
+                imgSrc: "/img-7.jpeg",
             },
         
 
@@ -28,11 +32,11 @@ const Events = () => {
 
             <div className="space-y-8">
                 {eventList.map((event, index) => (
-                    <div className="hero bg-base-20 border border-black-600" key={index}>
-                        <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div className="hero bg-base-20 border border-black-600 shadow-md" key={index}>
+                        <div className="hero-content flex-col lg:flex-row-reverse gap-6 w-full">
                             <img
-                                src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-                                className="max-w-sm rounded-lg shadow-2xl"
+                                src={event.imgSrc}
+                                className="max-w-sm h-48 rounded-lg shadow-md object-cover"
                             />
                             <div>
                                 <h1 className="text-5xl font-bold">{event.title}</h1>

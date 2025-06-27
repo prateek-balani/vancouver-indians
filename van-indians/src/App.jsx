df4c4c5172015a1f0e4ce7e017baa3ac4d196807
prateek-balani <prateekbalani03@gmail.com>
2025-06-27 14:16:17 -0700

    fixed the css

--- van-indians/src/App.jsx
+++ van-indians/src/App.jsx
@@ -11,19 +11,26 @@ import Footer from './components/footer';
 
 function App() {
   return (
-    <div className="min-h-screen w-full bg-gradient-to-t from-green-300 via-transparent to-orange-300">
-      <Router>
-        <>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/gallery" element={<Gallery />} />
-            <Route path="/faq" element={<FAQ />} />
-            <Route path="/events" element={<Events />} />
-          </Routes>
-          <Footer />
-        </>
-      </Router>
+    // <div className="min-h-screen w-full bg-gradient-to-t from-green-300 via-transparent to-orange-300">
+    <div className="relative min-h-screen w-full overflow-hidden">
+      <div className="absolute inset-0 bg-gradient-to-b from-orange-400 via-white to-green-500 z-0" />
+      <div className="absolute top-1/2 left-1/2 w-64 h-64 bg-blue-500 opacity-10 blur-3xl rounded-full transform -translate-x-1/2 -translate-y-1/2 z-0" />
+      <div className="relative z-10 flex flex-col min-h-screen">
+        <Router>
+          <>
+            <Navbar />
+            <main className="flex-grow">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/gallery" element={<Gallery />} />
+                <Route path="/faq" element={<FAQ />} />
+                <Route path="/events" element={<Events />} />
+              </Routes>
+            </main>
+            <Footer />
+          </>
+        </Router>
+      </div>
     </div>
 
 

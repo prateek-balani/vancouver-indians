227ccf61f1b2a4d8d25f64e8116f2d4b018f40e1
prateek-balani <prateekbalani03@gmail.com>
2025-06-27 14:20:40 -0700

    some more css changes

--- van-indians/src/pages/Home.jsx
+++ van-indians/src/pages/Home.jsx
@@ -41,19 +41,19 @@ const Home = () => {
             </section>
             <section className='px-6 py-12 max-w-screen-lg mx-auto'>
                 <h2 className='text-4xl font-bold text-center mb-10'>About</h2>
-                <div className='card bg-base-100 shadow-md p-6'>
+                <div className='card bg-base-100 shadow-md p-6 m-12'>
                     <h3 className='text-lg font-bold'>Who we are</h3>
                     <p className="text-lg mt-2 text-gray-600">
                         Vancouver Indians Group was founded by <b>Sunil Mantri</b> 16 years ago and is a grassroots community built by and for Indian immigrants living in the Greater Vancouver Area. Whether you’ve just landed or have been here for years, our goal is to make sure you never feel like you’re navigating this new chapter alone. We’re here to share resources, celebrate our culture, and help you find your people.
                     </p>
                 </div>
-                <div className='card bg-base-100 shadow-md p-6'>
+                <div className='card bg-base-100 shadow-md p-6 m-12'>
                     <h3 className='text-lg font-bold'>What we do</h3>
                     <p className="text-lg mt-2 text-gray-600">
                         From curated WhatsApp groups to local meetups, cultural events, job boards, and food recommendations — we help Indian newcomers build a meaningful life in Vancouver. Our goal is to bridge the gap between back home and your new home, by offering a platform that connects students, working professionals, and families alike.
                     </p>
                 </div>
-                <div className='card bg-base-100 shadow-md p-6'>
+                <div className='card bg-base-100 shadow-md p-6 m-12'>
                     <h3 className='text-lg font-bold'>Why It Matters</h3>
                     <p className="text-lg mt-2 text-gray-600">
                         Immigrating isn’t easy. It comes with challenges, especially when you’re far from familiar faces. Vancouver Indians was created to make that transition smoother — to help you build friendships, share experiences, and embrace the best of both worlds without losing your identity. Our strength lies in our unity, and we welcome everyone who wants to be part of something bigger than just themselves.

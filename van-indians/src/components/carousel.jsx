c37c8ffb54afcc1c006a5862a4b46fe9ff2f03df
prateek-balani <prateekbalani03@gmail.com>
2025-06-27 12:07:30 -0700

    Added the logo to the carousel

--- van-indians/src/components/carousel.jsx
+++ van-indians/src/components/carousel.jsx
@@ -1,50 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react';
+
+const images = [
+  { src: "vi-logo.jpg", alt: "Slide 1" },
+  { src: "img-18.JPG", alt: "Slide 2" },
+  { src: "img-15.JPG", alt: "Slide 3" },
+  { src: "img-11.JPG", alt: "Slide 4" },
+];
 
 const Carousel = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
 
-    return (
-        <div className='flex justify-center'>
-            <div className="carousel mx-auto mt-6 w-full max-w-4xl h-96 rounded-xl overflow-hidden shadow-md">
-                <div id="slide1" className="carousel-item relative w-full">
-                    <img
-                        src="/img-1.jpg"
-                        className="w-full h-full object-cover" />
-                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                        <a href="#slide4" className="btn btn-circle">❮</a>
-                        <a href="#slide2" className="btn btn-circle">❯</a>
-                    </div>
-                </div>
-                <div id="slide2" className="carousel-item relative w-full">
-                    <img
-                        src="/img-2.jpg"
-                        className="w-full h-full object-cover" />
-                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                        <a href="#slide1" className="btn btn-circle">❮</a>
-                        <a href="#slide3" className="btn btn-circle">❯</a>
-                    </div>
-                </div>
-                <div id="slide3" className="carousel-item relative w-full">
-                    <img
-                        src="/img-3.jpg"
-                        className="w-full h-full object cover" />
-                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                        <a href="#slide2" className="btn btn-circle">❮</a>
-                        <a href="#slide4" className="btn btn-circle">❯</a>
-                    </div>
-                </div>
-                <div id="slide4" className="carousel-item relative w-full">
-                    <img
-                        src="/img-4.jpg"
-                        className="w-full h-full object cover" />
-                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                        <a href="#slide3" className="btn btn-circle">❮</a>
-                        <a href="#slide1" className="btn btn-circle">❯</a>
-                    </div>
-                </div>
-            </div>
+  const handlePrev = () => {
+    setCurrentSlide((prev) => (prev === 0 ? images.length - 1 : prev - 1));
+  };
+
+  const handleNext = () => {
+    setCurrentSlide((prev) => (prev === images.length - 1 ? 0 : prev + 1));
+  };
+
+  return (
+    <div className="flex justify-center mt-6">
+      <div className="relative w-full max-w-4xl h-96 overflow-hidden rounded-xl shadow-md">
+        <div
+          className="flex transition-transform duration-500 ease-in-out"
+          style={{ transform: `translateX(-${currentSlide * 100}%)` }}
+        >
+          {images.map((img, index) => (
+            <img
+              key={index}
+              src={img.src}
+              alt={img.alt}
+              className="w-full h-96 flex-shrink-0"
+            />
+          ))}
         </div>
-    );
 
-}
+        <div className="absolute top-1/2 left-0 right-0 flex justify-between px-4 -translate-y-1/2">
+          <button onClick={handlePrev} className="btn btn-circle">❮</button>
+          <button onClick={handleNext} className="btn btn-circle">❯</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Carousel
+export default Carousel;

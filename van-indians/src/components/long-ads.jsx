569e2089029f25ace1270078fbdcc88341fa3c29
prateek-balani <prateekbalani03@gmail.com>
2025-06-28 18:10:43 -0700

    added conditionals for ads

--- van-indians/src/components/long-ads.jsx
+++ van-indians/src/components/long-ads.jsx
@@ -8,31 +8,37 @@ const LongAds = () => {
     ];
     return (
         <>
-            {/* for desktop */}
-            <div className='hidden xl:block absolute w-60 left-1 top-80 h-full shrink-0 pr-4'>
-                <div className="space-y-4 sticky top-80 ">
-                    {ads.map((ad, index) => (
-                        <div key={index} className="h-[500px] shadow-lg overflow-hidden">
-                            <img
-                                src={ad.link}
-                                alt={`ad-${index}`}
-                                className="w-full h-full object-cover"
-                            />
+            {ads.length > 0 ? (
+                <>
+                    {/* for desktop */}
+                    < div className='hidden xl:block absolute w-60 left-1 top-80 h-full shrink-0 pr-4'>
+                        <div className="space-y-4 sticky top-80 ">
+                            {ads.map((ad, index) => (
+                                <div key={index} className="h-[500px] shadow-lg overflow-hidden">
+                                    <img
+                                        src={ad.link}
+                                        alt={`ad-${index}`}
+                                        className="w-full h-full object-cover"
+                                    />
+                                </div>
+                            ))}
                         </div>
-                    ))}
-                </div>
-            </div>
-            {/* for mobile */}
-            <div className="xl:hidden w-full overflow-x-auto flex gap-4 px-4 py-2 bg-orange-100">
-                {ads.map((ad, index) => (
-                    <img
-                        key={index}
-                        src={ad.link}
-                        alt={`ad-mobile-${index}`}
-                        className="w-full h-full object-cover"
-                    />
-                ))}
-            </div>
+                    </div >
+                    {/* for mobile */}
+                    < div className="xl:hidden w-full overflow-x-auto flex gap-4 px-4 py-2 bg-orange-100" >
+                        {
+                            ads.map((ad, index) => (
+                                <img
+                                    key={index}
+                                    src={ad.link}
+                                    alt={`ad-mobile-${index}`}
+                                    className="w-full h-full object-cover"
+                                />
+                            ))
+                        }
+                    </div >
+                </>
+            ) : null}
         </>
     );
 }
--- van-indians/src/components/short-ads.jsx
+++ van-indians/src/components/short-ads.jsx
@@ -15,32 +15,36 @@ const ShortAds = () => {
     ];
     return (
         <>
-            {/* for desktop devices */}
-            <div className='hidden xl:block absolute w-40 right-1 top-80 h-full shrink-0 pr-4'>
-                <div className="space-y-4 sticky top-80 ">
-                    {ads.map((ad, index) => (
-                        <div key={index} className="h-[80px] shadow-lg overflow-hidden">
-                            <img
-                                src={ad.link}
-                                alt={`ad-${index}`}
-                                className="w-full h-full object-cover"
-                            />
+            {ads.length > 0 ? (
+                <>
+                    {/* for desktop devices */}
+                    <div className='hidden xl:block absolute w-40 right-1 top-80 h-full shrink-0 pr-4'>
+                        <div className="space-y-4 sticky top-80 ">
+                            {ads.map((ad, index) => (
+                                <div key={index} className="h-[80px] shadow-lg overflow-hidden">
+                                    <img
+                                        src={ad.link}
+                                        alt={`ad-${index}`}
+                                        className="w-full h-full object-cover"
+                                    />
+                                </div>
+                            ))}
                         </div>
-                    ))}
-                </div>
-            </div>
+                    </div>
 
-            {/* for mobile devices */}
-            <div className="xl:hidden w-full overflow-x-auto flex gap-2 px-4 py-2 bg-orange-100">
-                {ads.map((ad, index) => (
-                    <img
-                        key={index}
-                        src={ad.link}
-                        alt={`ad-mobile-${index}`}
-                        className="h-20 w-32 flex-shrink-0 rounded shadow"
-                    />
-                ))}
-            </div>
+                    {/* for mobile devices */}
+                    <div className="xl:hidden w-full overflow-x-auto flex gap-2 px-4 py-2 bg-orange-100">
+                        {ads.map((ad, index) => (
+                            <img
+                                key={index}
+                                src={ad.link}
+                                alt={`ad-mobile-${index}`}
+                                className="h-20 w-32 flex-shrink-0 rounded shadow"
+                            />
+                        ))}
+                    </div>
+                </>
+            ) : null}
         </>
     );
 }

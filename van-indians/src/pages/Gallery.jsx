2d8cb6ffee2d2906af057402fd7fe739947a0e15
prateek-balani <prateekbalani03@gmail.com>
2025-08-03 19:07:24 -0700

    renoved unused images

--- van-indians/src/pages/Gallery.jsx
+++ van-indians/src/pages/Gallery.jsx
@@ -2,78 +2,78 @@ import React from 'react';
 
 const Gallery = () => {
     const imgs = [
-        {
-            imageLink: "img-1.JPG",
-            caption: "Volleyball Meet Up"
-        },
-        {
-            imageLink: "img-2.JPG",
-            caption: "Sunday lunch"
-        },
-        {
-            imageLink: "img-3.JPG",
-            caption: "Sunday lunch"
-        },
-        {
-            imageLink: "img-4.JPG",
-            caption: "Sunday lunch"
-        },
-        {
-            imageLink: "img-5.JPG",
-            caption: "Coffee Meetup"
-        },
-        {
-            imageLink: "img-6.JPG",
-            caption: "Sunday lunch"
-        },
-        {
-            imageLink: "img-7.JPG",
-            caption: "Saturday Picnic and Potluck"
-        },
-        {
-            imageLink: "img-8.JPG",
-            caption: "Saturday Picnic and Potluck"
-        },
-        {
-            imageLink: "img-9.JPG",
-            caption: "Sunday Lunch"
-        },
-        {
-            imageLink: "img-10.JPG",
-            caption: "Sunday Lunch"
-        },
-        {
-            imageLink: "img-11.JPG",
-            caption: "Sunday Lunch"
-        },
-        {
-            imageLink: "img-12.JPG",
-            caption: "Sunday Lunch"
-        },
-        {
-            imageLink: "img-13.JPG",
-            caption: "Coffee Meetup"
-        },
-        {
-            imageLink: "img-14.JPG",
-            caption: "Saturday Picnic and Potluck"
-        },
-        {
-            imageLink: "img-15.JPG",
-            caption: "Lovely cake arranged by one of the members(Poonam Mittal) with Vancouver Indians logo on it"
-        },
-        {
-            imageLink: "img-16.JPG",
-            caption: "Saturday Picnic and Potluck"
-        },
-        {
-            imageLink: "img-17.JPG",
-            caption: "Coffee meetup"
-        },
-        {
-            imageLink: "img-18.JPG",
-            caption: "Coffee meetup"
-        }
+        // {
+        //     imageLink: "img-1.JPG",
+        //     caption: "Volleyball Meet Up"
+        // },
+        // {
+        //     imageLink: "img-2.JPG",
+        //     caption: "Sunday lunch"
+        // },
+        // {
+        //     imageLink: "img-3.JPG",
+        //     caption: "Sunday lunch"
+        // },
+        // {
+        //     imageLink: "img-4.JPG",
+        //     caption: "Sunday lunch"
+        // },
+        // {
+        //     imageLink: "img-5.JPG",
+        //     caption: "Coffee Meetup"
+        // },
+        // {
+        //     imageLink: "img-6.JPG",
+        //     caption: "Sunday lunch"
+        // },
+        // {
+        //     imageLink: "img-7.JPG",
+        //     caption: "Saturday Picnic and Potluck"
+        // },
+        // {
+        //     imageLink: "img-8.JPG",
+        //     caption: "Saturday Picnic and Potluck"
+        // },
+        // {
+        //     imageLink: "img-9.JPG",
+        //     caption: "Sunday Lunch"
+        // },
+        // {
+        //     imageLink: "img-10.JPG",
+        //     caption: "Sunday Lunch"
+        // },
+        // {
+        //     imageLink: "img-11.JPG",
+        //     caption: "Sunday Lunch"
+        // },
+        // {
+        //     imageLink: "img-12.JPG",
+        //     caption: "Sunday Lunch"
+        // },
+        // {
+        //     imageLink: "img-13.JPG",
+        //     caption: "Coffee Meetup"
+        // },
+        // {
+        //     imageLink: "img-14.JPG",
+        //     caption: "Saturday Picnic and Potluck"
+        // },
+        // {
+        //     imageLink: "img-15.JPG",
+        //     caption: "Lovely cake arranged by one of the members(Poonam Mittal) with Vancouver Indians logo on it"
+        // },
+        // {
+        //     imageLink: "img-16.JPG",
+        //     caption: "Saturday Picnic and Potluck"
+        // },
+        // {
+        //     imageLink: "img-17.JPG",
+        //     caption: "Coffee meetup"
+        // },
+        // {
+        //     imageLink: "img-18.JPG",
+        //     caption: "Coffee meetup"
+        // }
     ]
     const columns = 4;
     const columnImgs = Array.from({ length: columns }, (_, i) => imgs.filter((_, index) => index % columns === i));
